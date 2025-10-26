8d474e6e650fbef364b9e38d75bc8a8b0d532e24
yunajoe <yunajoe@gmail.com>
2025-10-26 19:50:30 +0900

    chore: zustand, sass 환경셋팅

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    implementation: "sass-embedded",
+  },
 };
 
 export default nextConfig;
